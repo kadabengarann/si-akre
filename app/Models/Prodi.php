dc58f0bf063ef1e05e2b19e6de4988a9e201523b
kadabengarann <rio27dh@gmail.com>
2021-10-22 00:59:05 +0800

    add prodi model

---
+++ app/Models/Prodi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Prodi extends Model
+{
+    use HasFactory;
+    protected $table = 'prodi';
+    protected $fillable = ['nama'];
+
+
+    public function getProdiID()
+    {
+        return sprintf('PR%03d', $this->id);
+    }
+    public function dosen()
+    {
+        return $this->hasMany(Dosen::class);
+    }
+}
