6810c21ea4925a12de0333939b4a489d287525b3
kadabengarann <rio27dh@gmail.com>
2022-01-27 07:10:04 +0800

    apply auditable

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
 
-class Dosen extends Model
+class Dosen extends Model implements Auditable
 {
-    // use \OwenIt\Auditing\Auditable;
+    use \OwenIt\Auditing\Auditable;
     use HasFactory;
 
     protected $table = 'dosen';
