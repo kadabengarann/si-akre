40604ea1e53ba6cdf3d72f97a8f30cb82d4634ad
kadabengarann <rio27dh@gmail.com>
2022-01-31 10:18:55 +0800

    add text navbar

--- resources/views/layouts/nav/lkps.blade.php
+++ resources/views/layouts/nav/lkps.blade.php
@@ -80,49 +80,49 @@
         <li class="nav-item">
             <a href="/lkps/view/411" class="nav-link {{ request()->is('lkps/*/411') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. a</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/412" class="nav-link {{ request()->is('lkps/*/412') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. b</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/413" class="nav-link {{ request()->is('lkps/*/413') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. c</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/414" class="nav-link {{ request()->is('lkps/*/414') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. d</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/415" class="nav-link {{ request()->is('lkps/*/415') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. e</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/416" class="nav-link {{ request()->is('lkps/*/416') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. f</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/417" class="nav-link {{ request()->is('lkps/*/417') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. g</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/418" class="nav-link {{ request()->is('lkps/*/418') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>4. h</p>
             </a>
         </li>
     </ul>
@@ -138,49 +138,49 @@
         <li class="nav-item">
             <a href="/lkps/view/511" class="nav-link {{ request()->is('lkps/*/511') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. a</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/512" class="nav-link {{ request()->is('lkps/*/512') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. b</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/513" class="nav-link {{ request()->is('lkps/*/513') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. c</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/514" class="nav-link {{ request()->is('lkps/*/514') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. d</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/515" class="nav-link {{ request()->is('lkps/*/515') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. e</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/516" class="nav-link {{ request()->is('lkps/*/516') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. f</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/517" class="nav-link {{ request()->is('lkps/*/517') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. g</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/518" class="nav-link {{ request()->is('lkps/*/518') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>5. h</p>
             </a>
         </li>
     </ul>
@@ -196,49 +196,49 @@
         <li class="nav-item">
             <a href="/lkps/view/611" class="nav-link {{ request()->is('lkps/*/611') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. a</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/612" class="nav-link {{ request()->is('lkps/*/612') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. b</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/613" class="nav-link {{ request()->is('lkps/*/613') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. c</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/614" class="nav-link {{ request()->is('lkps/*/614') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. e</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/615" class="nav-link {{ request()->is('lkps/*/615') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. f</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/616" class="nav-link {{ request()->is('lkps/*/616') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. g</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/617" class="nav-link {{ request()->is('lkps/*/617') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. h</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/618" class="nav-link {{ request()->is('lkps/*/618') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>6. i</p>
             </a>
         </li>
     </ul>
@@ -254,7 +254,7 @@
         <li class="nav-item">
             <a href="/lkps/view/711" class="nav-link {{ request()->is('lkps/*/711') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>7. a</p>
             </a>
         </li>
     </ul>
@@ -270,7 +270,7 @@
         <li class="nav-item">
             <a href="/lkps/view/811" class="nav-link {{ request()->is('lkps/*/811') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>8. a</p>
             </a>
         </li>
     </ul>
@@ -287,55 +287,55 @@
         <li class="nav-item">
             <a href="/lkps/view/911" class="nav-link {{ request()->is('lkps/*/911') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. a</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/912" class="nav-link {{ request()->is('lkps/*/912') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. b</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/913" class="nav-link {{ request()->is('lkps/*/913') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. c</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/914" class="nav-link {{ request()->is('lkps/*/914') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. d</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/915" class="nav-link {{ request()->is('lkps/*/915') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. e</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/916" class="nav-link {{ request()->is('lkps/*/916') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. f</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/917" class="nav-link {{ request()->is('lkps/*/917') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. g</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/918" class="nav-link {{ request()->is('lkps/*/918') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. h</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="/lkps/view/919" class="nav-link {{ request()->is('lkps/*/919') ? 'active' : '' }}">
                 <i class="far fa-circle nav-icon"></i>
-                <p></p>
+                <p>9. i</p>
             </a>
         </li>
     </ul>
