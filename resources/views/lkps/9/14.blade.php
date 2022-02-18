6a8223d066be8ec54fe4ddf71b2d2ce12d3926ae
komputasiawan <komputasiawan6@gmail.com>
2022-02-18 13:43:19 +0800

    update and rename lkps 9

--- resources/views/lkps/9/14.blade.php
+++ resources/views/lkps/9/14.blade.php
@@ -10,8 +10,7 @@
                         <p><b>Kriteria : </b>9. Luaran dan Capaian Tridharma</p>
                     </div>
                     <div class="col-12 col-lg-6">
-                        <p><b>Sub-kriteria : </b>a. Capaian Pembelajaran</p>
-                        <p><b>Tabel : </b>9.6. Kelulusan Tepat Waktu
+                        <p><b>Tabel : </b>9.14. Penjaringan Umpan Balik terkait Pelaksanaan Tri Dharma Perguruan Tinggi
                         </p>
                     </div>
                 </div>
@@ -22,88 +21,106 @@
         </div>
         <div class="card card-primary card-outline">
             <div class="card-header">
-                <h3 class="text-center card-title" style="float: none; font-weight:500">IPK Lulusan</h3>
+                <h3 class="text-center card-title" style="float: none; font-weight:500">Penjaringan Umpan Balik terkait
+                    Pelaksanaan Tri Dharma Perguruan Tinggi
+                </h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body">
-                <table id="tbl_list" class="table table-striped table-bordered table-center-text">
+                <table id="tbl_list" class="table table-bordered table-center-text">
                     <thead>
                         <tr>
-                            <th rowspan="2">Tahun Masuk</th>
-                            <th rowspan="2">Jumlah Mahasiswa Diterima</th>
-                            <th colspan="7">Jumlah Mahasiswa yang lulus pada</th>
-                            <th rowspan="2">Jumlah Lulusan s.d. Akhir TS</th>
-                            <th rowspan="2">Rata-rata Masa Studi</th>
-                            <th rowspan="2">Jumlah mhs. Yang masih aktif </th>
+                            <th class="col-4">Sumber Umpan Balik
+                            </th>
+                            <th>Isi Umpan Balik
+
+                            </th>
+                            <th>Cara memperoleh umpan balik
 
 
-                        </tr>
-                        <tr>
-                            <th>Akhir TS-6</th>
-                            <th>Akhir TS-5</th>
-                            <th>Akhir TS-4</th>
-                            <th>Akhir TS-3</th>
-                            <th>Akhir TS-2</th>
-                            <th>Akhir TS-1</th>
-                            <th>Akhir TS</th>
+                            </th>
+                            <th>Jenis Tindak Lanjut
 
+                            </th>
+                            <th>Waktu (tgl/Bln/Thn)
+
+                            </th>
+                            <th>Bukti
+
+                            </th>
+                            <th>Keterangan
+
+                            </th>
+                            <th>Bukti
+                            </th>
+                            <th>Keterangan
+                            </th>
                         </tr>
                     </thead>
+                    <tr>
+                        <th class="sub-table">
+                            1. Dosen
+                        </th>
+                    </tr>
+                    <tbody>
+                        <td></td>
+
+                        <td colspan="9" class="table-isi">
+                            No data
+                        </td>
+                        </tr>
+                    </tbody>
+                    <tr>
+                        <th class="sub-table">
+                            2. Mahasiswa
+
+                        </th>
+                    </tr>
                     <tbody>
                         <tr>
-                            <td>1</td>
-                            <td>2</td>
-                            <td>3</td>
-                            <td>4</td>
-                            <td>5</td>
-                            <td>6</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>9</td>
-                            <td>10</td>
-                            <td>11</td>
-                            <td>12</td>
+                            <td></td>
 
-
-                        </tr>
-                        <tr>
-                            <td>TS-6</td>
-                            <td colspan="12">
-                                No data
-                            </td>
-
-
-
-                        </tr>
-                        <tr>
-                            <td>TS-5</td>
-
-                            <td colspan="12">
-                                No data
-                            </td>
-
-                        </tr>
-                        <tr>
-                            <td>TS-4</td>
-                            <td colspan="12">
-                                No data
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>TS-3</td>
-                            <td colspan="12">
+                            <td colspan="9" class="table-isi">
                                 No data
                             </td>
                         </tr>
                     </tbody>
+                    <tr>
+                        <th class="sub-table">
+                            3. Alumni
+                        </th>
+                    </tr>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td colspan="9" class="table-isi">
+                                No data
+                            </td>
+                        </tr>
+                    </tbody>
+                    <tr>
+                        <th class="sub-table">
+                            4. Pengguna Lulusan</th>
+                    </tr>
+                    <tbody>
+                        <tr>
+                            <td></td>
+
+                            <td colspan="9" class="table-isi">
+                                No data
+                            </td>
+                        </tr>
+                    </tbody>
+
                 </table>
             </div>
 
             <div class="form-group d-flex align-items-center justify-content-between mb-4 ml-4">
-                <a class="btn btn-primary" href="/lkps/input/914"><i class="fas fa-plus-circle"></i> Input data</a>
+                <a class="btn btn-primary" href="/lkps/input/918"><i class="fas fa-plus-circle"></i> Input data</a>
             </div>
 
             <!-- /.card-body -->
         </div>
         <!-- /.card -->
-</section> @endsection
+    </section>
+@endsection
